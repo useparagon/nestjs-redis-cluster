aae86186b80d7ba1c2cf9a6f6d96dffc4f569ddc
Nipun Dawar <nipun.dawar@useparagon.com>
2024-06-20 11:48:32 +0530

    (feat)-PARA-10748: add option to restrict client closure on shutdown

--- lib/cluster.service.ts
+++ lib/cluster.service.ts
@@ -1,13 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
 import * as IORedis from 'ioredis';
 
-import { REDIS_CLUSTER } from './cluster.constants';
+import { REDIS_CLUSTER, REDIS_CLUSTER_MODULE_OPTIONS } from './cluster.constants';
 import { RedisClusterProvider, RedisClusterError } from './cluster.provider';
+import { RedisClusterModuleOptions } from './cluster.interface';
 
 @Injectable()
 export class RedisClusterService {
   constructor(
-    @Inject(REDIS_CLUSTER) private readonly provider: RedisClusterProvider,
+    @Inject(REDIS_CLUSTER)
+    private readonly provider: RedisClusterProvider,
+
+    @Inject(REDIS_CLUSTER_MODULE_OPTIONS)
+    private readonly options: RedisClusterModuleOptions | RedisClusterModuleOptions[],
   ) {}
 
   getCluster(name?: string): IORedis.Cluster {
@@ -23,5 +28,26 @@ export class RedisClusterService {
 
   getClusters(): Map<string, IORedis.Cluster> {
     return this.provider.clusters;
+  }
+
+  disconnectAllClients(): void {
+    const closeConnection =
+      ({ clusters, defaultKey }: RedisClusterProvider) =>
+      (options: RedisClusterModuleOptions) => {
+        const name = options.name || defaultKey;
+        const cluster: IORedis.Cluster = clusters.get(name) as IORedis.Cluster;
+
+        if (cluster) {
+          cluster.disconnect();
+        }
+      };
+
+    const closeClusterConnection = closeConnection(this.provider);
+
+    if (Array.isArray(this.options)) {
+      this.options.forEach(closeClusterConnection);
+    } else {
+      closeClusterConnection(this.options);
+    }
   }
 }
--- lib/redis.interface.ts
+++ lib/redis.interface.ts
@@ -2,9 +2,26 @@ import { ModuleMetadata } from '@nestjs/common/interfaces';
 import { Redis, RedisOptions } from 'ioredis';
 
 export interface RedisModuleOptions extends RedisOptions {
+  /**
+   * redis connection anme
+   */
   name?: string;
+
+  /**
+   * redis connection url
+   */
   url?: string;
+
+  /**
+   * callback to execute once client has connected and is in ready state
+   */
   onClientReady?(client: Redis): Promise<void>;
+
+  /**
+   * when `true`, doesn't close the redis connections on app shutdown, instead
+   * delegates connection closure responsibility to the consumer via redisService#disconnectAllClients
+   */
+  disableDisconnectOnShutdown?: boolean;
 }
 
 export interface RedisModuleAsyncOptions
